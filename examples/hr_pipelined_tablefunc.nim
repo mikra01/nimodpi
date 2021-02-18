611f6b9da87e6a1585545f121042be5f4cab4517
Michael Krauter <mtkrauter@gmail.com>
2021-02-18 22:06:00 +0100

    row to object handling added

--- examples/hr_pipelined_tablefunc.nim
+++ examples/hr_pipelined_tablefunc.nim
@@ -11,7 +11,7 @@ include ora_credentials
 
 var 
     hr_pa_employee_spec : SqlQuery = osql""" 
-CREATE OR REPLACE PACKAGE hr.pa_employee AS
+CREATE OR REPLACE PACKAGE hr.test_pa_employee AS
     TYPE ty_employee_rec IS RECORD (
         employee_id          NUMBER(6, 0),
         first_name           VARCHAR2(20 BYTE),
@@ -31,10 +31,10 @@ CREATE OR REPLACE PACKAGE hr.pa_employee AS
         department_id_in NUMBER
     ) RETURN ty_employees
         PIPELINED;
-END pa_employee;
+END test_pa_employee;
     """
     hr_pa_employee_body : SqlQuery = osql"""
-CREATE OR REPLACE PACKAGE BODY hr.pa_employee AS
+CREATE OR REPLACE PACKAGE BODY hr.test_pa_employee AS
     FUNCTION getemployeesby_department_id (
         department_id_in NUMBER
     ) RETURN ty_employees
@@ -74,13 +74,13 @@ CREATE OR REPLACE PACKAGE BODY hr.pa_employee AS
         CLOSE employee_cur;
     END getemployeesby_department_id;
 
-END pa_employee;
+END test_pa_employee;
     """
     octx: OracleContext
 
 type
     EmployeeDbRec = object 
-        employee_id : Option[int] 
+        employee_id : Option[int64] 
         first_name : Option[string]           
         last_name : Option[string]            
         email : Option[string]                
@@ -88,9 +88,28 @@ type
         hire_date : Option[DateTime] 
         job_id  : Option[string]            
         job_title : Option[string]           
-        manager_id : Option[int]          
+        manager_id : Option[int64]          
         manager_first_name : Option[string]  
         manager_last_name : Option[string]   
+
+template toEmployee( row : DpiRow ) : EmployeeDbRec =
+  EmployeeDbRec(employee_id : row["EMPLOYEE_ID"].fetchInt64, 
+                first_name : row["FIRST_NAME"].fetchString,           
+                last_name : row["LAST_NAME"].fetchString,            
+                email : row["EMAIL"].fetchString,               
+                phone_number : row["PHONE_NUMBER"].fetchString,         
+                hire_date : row["HIRE_DATE"].fetchDateTime ,
+                job_id  : row["JOB_ID"].fetchString ,          
+                job_title : row["JOB_TITLE"].fetchString,           
+                manager_id : row["MANAGER_ID"].fetchInt64,         
+                manager_first_name : row["MANAGER_FIRST_NAME"].fetchString, 
+                manager_last_name : row["MANAGER_LAST_NAME"].fetchString)
+  # FIXME: autogen this part with macro              
+
+proc `$`*(p: EmployeeDbRec): string =
+     $p.first_name & " " & $p.last_name & " " & $p.email & " " & $p.phone_number &
+     " " & $p.hire_date & " " & $p.job_id & " " & $p.job_title & " " & $p.manager_id & 
+     " " & $p.manager_first_name & " " & $p.manager_last_name
 
 newOracleContext(octx,DpiAuthMode.SYSDBA)
 # create the context with authentication Method
@@ -103,7 +122,7 @@ createConnection(octx, connectionstr, oracleuser, pw, conn)
 
 conn.executeDDL(hr_pa_employee_spec)
 conn.executeDDL(hr_pa_employee_body)  
-echo "package hr.pa_employee created"
+echo "package hr.test_pa_employee created"
 
 var getEmpSql : SqlQuery = osql"""SELECT
     employee_id,
@@ -118,7 +137,7 @@ var getEmpSql : SqlQuery = osql"""SELECT
     manager_first_name,
     manager_last_name
 FROM
-    TABLE ( hr.pa_employee.getemployeesby_department_id( :department_id) )
+    TABLE ( hr.test_pa_employee.getemployeesby_department_id( :department_id) )
  """
 
 var pstmt : PreparedStatement
@@ -128,7 +147,7 @@ conn.newPreparedStatement(getEmpSql , pstmt, 5)
 # rows (window) is fixed and can't changed later on
 
 withPreparedStatement(pstmt):
-  # use withPreparedStatement to recycle it automatically
+  # use template "withPreparedStatement" to recycle it automatically
   # after leaving this block. 
   var rs: ResultSet
   var param = addBindParameter(pstmt,Int64ColumnTypeParam,"department_id")
@@ -143,26 +162,13 @@ withPreparedStatement(pstmt):
     # fit into entire window we are done here.
  
   for row in rs.resultSetRowIterator:
-      # the result iterator fetches internally further
-      # rows if present 
-      # example of value retrieval by columnname or index
-      # the column of the row is accessed by colnum or name
-      echo $row["EMPLOYEE_ID"].fetchInt64 &
-      " " & $row["FIRST_NAME"].fetchString &
-      " " & $row["LAST_NAME"].fetchString &
-      " " & $row["EMAIL"].fetchString &
-      " " & $row["PHONE_NUMBER"].fetchString &
-      " " & row["HIRE_DATE"].fetchDateTime.get.format("yyyy-MM-dd") & 
-      " " & $row["JOB_ID"].fetchInt64 &  
-      " " & $row["JOB_TITLE"].fetchString &
-      " " & $row["MANAGER_ID"].fetchInt64 &    
-      " " & $row["MANAGER_FIRST_NAME"].fetchString &
-      " " & $row["MANAGER_LAST_NAME"].fetchString  
-      # columns can be accessed by column-index or column_name
+      echo $row.toEmployee 
 
-var hr_pa_employee_drop : SqlQuery = osql"""drop package hr.pa_employee"""
-# conn.executeDDL(hr_pa_employee_drop)
-echo "package hr.pa_employee dropped"
+  # FIXME: bulk insert employee example
+
+var hr_pa_employee_drop : SqlQuery = osql"""drop package hr.test_pa_employee"""
+conn.executeDDL(hr_pa_employee_drop)
+echo "package hr.test_pa_employee dropped"
 
 conn.releaseConnection
 destroyOracleContext(octx)  
